c7b97f4da1525b557747763b71de514de68af113
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-10-28 13:18:31 +0100

    new modules deployment

--- src/deploy/modules.ts
+++ src/deploy/modules.ts
@@ -1,32 +1,32 @@
-import fs from 'fs';
-import { Provider, defaultProvider, Account, ec, json } from 'starknet';
-// import { utils } from '@snapshot-labs/sx';
+import fs from "fs";
+import { Provider, defaultProvider, Account, ec, json } from "starknet";
 
-const ARTIFACTS_PATH = './node_modules/@orland0x/sx-core/starknet-artifacts';
+const ARTIFACTS_PATH = "./node_modules/@orland0x/sx-core/starknet-artifacts";
 
 async function main() {
-
   // Obtained via declaring the space account contract:
   // starknet declare --contract ./starknet-artifacts/contracts/starknet/SpaceAccount.cairo/SpaceAccount.json
-  const spaceClassHash = "0x591469387a06fb88dd66559f4bc91e4c0c1e61e65c259b14a4d43f274fdabd9";
+  const spaceClassHash =
+    "0x591469387a06fb88dd66559f4bc91e4c0c1e61e65c259b14a4d43f274fdabd9";
 
-  const l1MessagesSenderAddress = '0x738bfb83246156b759165d244077865B994F9d33';
+  const starknetCommitAddress = "0x76AE9330aA4f807A2e134d2fb2FcBBAfB806985E";
+
+  const l1MessagesSenderAddress = "0x738bfb83246156b759165d244077865B994F9d33";
   const fossilFactRegistryAddress =
-    '0x363108ac1521a47b4f7d82f8ba868199bc1535216bbedfc1b071ae93cc406fd';
+    "0x363108ac1521a47b4f7d82f8ba868199bc1535216bbedfc1b071ae93cc406fd";
   const fossilL1HeadersStoreAddress =
-    '0x6ca3d25e901ce1fff2a7dd4079a24ff63ca6bbf8ba956efc71c1467975ab78f';
+    "0x6ca3d25e901ce1fff2a7dd4079a24ff63ca6bbf8ba956efc71c1467975ab78f";
 
-  const starknetCommitAddress = '0x76AE9330aA4f807A2e134d2fb2FcBBAfB806985E';
-
-  const provider = process.env.STARKNET_PROVIDER_BASE_URL === undefined ?
-  defaultProvider :
-    new Provider({
-      sequencer: {
-        baseUrl: process.env.STARKNET_PROVIDER_BASE_URL!,
-        feederGatewayUrl: 'feeder_gateway',
-        gatewayUrl: 'gateway',
-      }, 
-  });
+  const provider =
+    process.env.STARKNET_PROVIDER_BASE_URL === undefined
+      ? defaultProvider
+      : new Provider({
+          sequencer: {
+            baseUrl: process.env.STARKNET_PROVIDER_BASE_URL!,
+            feederGatewayUrl: "feeder_gateway",
+            gatewayUrl: "gateway",
+          },
+        });
 
   // const starkAccount = new Account(
   //   provider,
@@ -37,64 +37,126 @@ async function main() {
   const compiledVanillaAuthenticator = json.parse(
     fs
       .readFileSync(
-        ARTIFACTS_PATH+'/contracts/starknet/Authenticators/Vanilla.cairo/Vanilla.json'
+        ARTIFACTS_PATH +
+          "/contracts/starknet/Authenticators/Vanilla.cairo/Vanilla.json"
       )
-      .toString('ascii')
+      .toString("ascii")
   );
 
   const compiledEthSigAuthenticator = json.parse(
     fs
       .readFileSync(
-        ARTIFACTS_PATH+'/contracts/starknet/Authenticators/EthSig.cairo/EthSig.json'
+        ARTIFACTS_PATH +
+          "/contracts/starknet/Authenticators/EthSig.cairo/EthSig.json"
       )
-      .toString('ascii')
+      .toString("ascii")
+  );
+  const compiledEthTxAuthenticator = json.parse(
+    fs
+      .readFileSync(
+        ARTIFACTS_PATH +
+          "/contracts/starknet/Authenticators/EthTx.cairo/EthTx.json"
+      )
+      .toString("ascii")
+  );
+  const compiledStarkSigAuthenticator = json.parse(
+    fs
+      .readFileSync(
+        ARTIFACTS_PATH +
+          "/contracts/starknet/Authenticators/StarkSig.cairo/StarkSig.json"
+      )
+      .toString("ascii")
+  );
+  const compiledEthSigSessionKeyAuthenticator = json.parse(
+    fs
+      .readFileSync(
+        ARTIFACTS_PATH +
+          "/contracts/starknet/Authenticators/EthSigSessionKey.cairo/EthSigSessionKey.json"
+      )
+      .toString("ascii")
+  );
+  const compiledEthTxSessionKeyAuthenticator = json.parse(
+    fs
+      .readFileSync(
+        ARTIFACTS_PATH +
+          "/contracts/starknet/Authenticators/EthTxSessionKey.cairo/EthTxSessionKey.json"
+      )
+      .toString("ascii")
   );
   const compiledVanillaVotingStrategy = json.parse(
     fs
       .readFileSync(
-        ARTIFACTS_PATH+'/contracts/starknet/VotingStrategies/Vanilla.cairo/Vanilla.json'
+        ARTIFACTS_PATH +
+          "/contracts/starknet/VotingStrategies/Vanilla.cairo/Vanilla.json"
       )
-      .toString('ascii')
+      .toString("ascii")
   );
   const compiledEthBalanceOfVotingStrategy = json.parse(
     fs
       .readFileSync(
-        ARTIFACTS_PATH+'/contracts/starknet/VotingStrategies/EthBalanceOf.cairo/EthBalanceOf.json'
+        ARTIFACTS_PATH +
+          "/contracts/starknet/VotingStrategies/EthBalanceOf.cairo/EthBalanceOf.json"
       )
-      .toString('ascii')
+      .toString("ascii")
   );
   const compiledVanillaExecutionStrategy = json.parse(
     fs
       .readFileSync(
-        ARTIFACTS_PATH+'/contracts/starknet/ExecutionStrategies/Vanilla.cairo/Vanilla.json'
+        ARTIFACTS_PATH +
+          "/contracts/starknet/ExecutionStrategies/Vanilla.cairo/Vanilla.json"
       )
-      .toString('ascii')
+      .toString("ascii")
   );
-  const compiledZodiacExecutionStrategy = json.parse(
+  const compiledEthRelayerExecutionStrategy = json.parse(
     fs
       .readFileSync(
-        ARTIFACTS_PATH+'/contracts/starknet/ExecutionStrategies/EthRelayer.cairo/EthRelayer.json'
+        ARTIFACTS_PATH +
+          "/contracts/starknet/ExecutionStrategies/EthRelayer.cairo/EthRelayer.json"
       )
-      .toString('ascii')
+      .toString("ascii")
   );
   const compiledSpaceFactory = json.parse(
     fs
-      .readFileSync(ARTIFACTS_PATH+'/contracts/starknet/SpaceFactory.cairo/SpaceFactory.json')
-      .toString('ascii')
+      .readFileSync(
+        ARTIFACTS_PATH +
+          "/contracts/starknet/SpaceFactory.cairo/SpaceFactory.json"
+      )
+      .toString("ascii")
   );
   const compiledSpace = json.parse(
     fs
-      .readFileSync(ARTIFACTS_PATH+'/contracts/starknet/SpaceAccount.cairo/SpaceAccount.json')
-      .toString('ascii')
+      .readFileSync(
+        ARTIFACTS_PATH +
+          "/contracts/starknet/SpaceAccount.cairo/SpaceAccount.json"
+      )
+      .toString("ascii")
   );
 
   const deployTxs = [
     provider.deployContract({ contract: compiledVanillaAuthenticator }),
     provider.deployContract({ contract: compiledEthSigAuthenticator }),
+    provider.deployContract({
+      contract: compiledEthTxAuthenticator,
+      constructorCalldata: [starknetCommitAddress],
+    }),
+    provider.deployContract({ contract: compiledStarkSigAuthenticator }),
+    provider.deployContract({
+      contract: compiledEthSigSessionKeyAuthenticator,
+    }),
+    provider.deployContract({
+      contract: compiledEthTxSessionKeyAuthenticator,
+      constructorCalldata: [starknetCommitAddress],
+    }),
     provider.deployContract({ contract: compiledVanillaVotingStrategy }),
-    provider.deployContract({ contract: compiledEthBalanceOfVotingStrategy, constructorCalldata: [fossilFactRegistryAddress, fossilL1HeadersStoreAddress]  }),
+    provider.deployContract({
+      contract: compiledEthBalanceOfVotingStrategy,
+      constructorCalldata: [
+        fossilFactRegistryAddress,
+        fossilL1HeadersStoreAddress,
+      ],
+    }),
     provider.deployContract({ contract: compiledVanillaExecutionStrategy }),
-    provider.deployContract({ contract: compiledZodiacExecutionStrategy }),
+    provider.deployContract({ contract: compiledEthRelayerExecutionStrategy }),
     provider.deployContract({
       contract: compiledSpaceFactory,
       constructorCalldata: [spaceClassHash],
@@ -104,17 +166,32 @@ async function main() {
   console.log(responses);
   const vanillaAuthenticatorAddress = responses[0].contract_address!;
   const ethSigAuthenticatorAddress = responses[1].contract_address!;
-  const vanillaVotingStrategyAddress = responses[2].contract_address!;
-  const ethBalanceOfVotingStrategyAddress = responses[3].contract_address!;
-  const vanillaExecutionStrategyAddress = responses[4].contract_address!;
-  const zodiacExecutionStrategyAddress = responses[5].contract_address!;
-  const spaceFactoryAddress = responses[6].contract_address!;
+  const ethTxAuthenticatorAddress = responses[2].contract_address!;
+  const starkSigAuthenticatorAddress = responses[3].contract_address!;
+  const ethSigSessionKeyAuthenticatorAddress = responses[4].contract_address!;
+  const ethTxSessionKeyAuthenticatorAddress = responses[5].contract_address!;
+  const vanillaVotingStrategyAddress = responses[6].contract_address!;
+  const ethBalanceOfVotingStrategyAddress = responses[7].contract_address!;
+  const vanillaExecutionStrategyAddress = responses[8].contract_address!;
+  const ethRelayerExecutionStrategyAddress = responses[9].contract_address!;
+  const spaceFactoryAddress = responses[10].contract_address!;
 
   // Storing deployment config.
   const modules = {
+    version: "0.1.0-beta.3",
     authenticators: {
       vanilla: vanillaAuthenticatorAddress,
       ethSig: ethSigAuthenticatorAddress,
+      ethSigSessionKey: ethSigSessionKeyAuthenticatorAddress,
+      ethTx: {
+        address: ethTxAuthenticatorAddress,
+        starknetCommit: starknetCommitAddress,
+      },
+      ethTxSessionKey: {
+        address: ethTxSessionKeyAuthenticatorAddress,
+        starknetCommit: starknetCommitAddress,
+      },
+      starkSig: starkSigAuthenticatorAddress,
     },
     votingStrategies: {
       vanilla: vanillaVotingStrategyAddress,
@@ -127,7 +204,7 @@ async function main() {
     },
     executionStrategies: {
       vanilla: vanillaExecutionStrategyAddress,
-      zodiac: zodiacExecutionStrategyAddress,
+      zodiac: ethRelayerExecutionStrategyAddress,
     },
     spaceFactory: {
       address: spaceFactoryAddress,
@@ -135,8 +212,8 @@ async function main() {
     },
   };
 
-  fs.writeFileSync('./deployments/modules.json', JSON.stringify(modules));
-  console.log('---- MODULE DEPLOYMENT COMPLETE ----');
+  fs.writeFileSync("./deployments/modules.json", JSON.stringify(modules));
+  console.log("---- MODULE DEPLOYMENT COMPLETE ----");
 }
 
 main()
--- src/deploy/starknetCommit.ts
+++ src/deploy/starknetCommit.ts
@@ -1,22 +1,37 @@
-import fs from 'fs';
-import * as dotenv from 'dotenv';
-import { ethers } from 'ethers';
-import fetch from 'cross-fetch';
+import fs from "fs";
+import * as dotenv from "dotenv";
+import { ethers } from "ethers";
+import fetch from "cross-fetch";
 
 dotenv.config();
 
-const ARTIFACTS_PATH = './node_modules/@orland0x/sx-core/artifacts';
+const ARTIFACTS_PATH = "./node_modules/@orland0x/sx-core/artifacts";
 
 async function main() {
-    global.fetch = fetch;
-    const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_NODE_URL!);
-    const ethAccount = new ethers.Wallet(process.env.ETH_PK_1!, provider);
-    const starknetCoreAddress = "0xde29d060D45901Fb19ED6C6e959EB22d8626708e";
-    const metadata = JSON.parse(fs.readFileSync(ARTIFACTS_PATH+'/contracts/ethereum/StarkNetCommit/StarkNetCommit.sol/StarkNetCommit.json').toString())
+  global.fetch = fetch;
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.GOERLI_NODE_URL!
+  );
+  const ethAccount = new ethers.Wallet(process.env.ETH_PK_1!, provider);
+  const starknetCoreAddress = "0xde29d060D45901Fb19ED6C6e959EB22d8626708e";
+  const metadata = JSON.parse(
+    fs
+      .readFileSync(
+        ARTIFACTS_PATH +
+          "/contracts/ethereum/StarkNetCommit/StarkNetCommit.sol/StarkNetCommit.json"
+      )
+      .toString()
+  );
 
-    const starknetCommitFactory = new ethers.ContractFactory(metadata.abi, metadata.bytecode, ethAccount);
-    const starknetCommit = await starknetCommitFactory.deploy(starknetCoreAddress);
-    console.log(starknetCommit);
+  const starknetCommitFactory = new ethers.ContractFactory(
+    metadata.abi,
+    metadata.bytecode,
+    ethAccount
+  );
+  const starknetCommit = await starknetCommitFactory.deploy(
+    starknetCoreAddress
+  );
+  console.log(starknetCommit);
 }
 
 main()
